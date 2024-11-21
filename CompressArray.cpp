20f8d12e385d2d74021fdda67edbd35c59c667fb
Maria <lebedeva.maria@stud.onu.edu.ua>
2024-11-21 13:36:54 +0200

    Новые функции creatArray и printArray
    + size теперь глобальная константа

--- CompressArray.cpp
+++ CompressArray.cpp
@@ -22,7 +22,7 @@ int main() {
 
 void creatArray(int* arr) {
     for (int i = 0; i < size; ++i) {
-        arr[i] = rand() % size - size / 2;
+        arr[i] = rand() % size + 1;
     }
 }
 void printArray(int* arr) {
